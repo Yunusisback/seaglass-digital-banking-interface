6fd9b7882db601dfaf2e47923d91457132327322
Yunusisback <yunusyertum1@gmail.com>
2025-12-26 00:19:32 +0300

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -44,42 +44,42 @@ export const destinations = [
   {
     name: "Maldivler",
     image: "https://cdn.forevervacation.com/uploads/blog/everything-you-need-to-know-about-the-maldives-the-vacationer-by-forevervacation-2003.jpeg?w=800",
-    discount: "%25",
+    discount: "%45",
     hotels: 45,
     description: "Turkuaz lagünler ve su üstü villalar"
   },
   {
     name: "Bahamalar",
     image: "https://www.bahamalar.tr/wp-content/uploads/2024/11/bahamalar-nassau.jpg?w=800",
-    discount: "%20",
+    discount: "%40",
     hotels: 120,
     description: "Pembe kumlu plajlar ve kristal sular"
   },
   {
     name: "Antalya",
     image: "https://images.contentstack.io/v3/assets/blt06f605a34f1194ff/blt21155ac1ca63f676/687f9d115903b3a04b04e07c/BCC-2024-EXPLORER-ANTALYA-FREE_THINGS_TO_DO-HEADER-MOBILE.jpg?format=webp&quality=60&width=1440?w=800",
-    discount: "%30",
+    discount: "%50",
     hotels: 38,
     description: "Antik kentler ve masmavi Akdeniz kıyıları"
   },
   {
   name: "Marsilya",
   image: "https://www.buseterim.com.tr/upload/default/2024/10/11/6801.jpg?w=800",
-  discount: "%22",
+  discount: "%30",
   hotels: 67,
   description: "Akdeniz limanı, tarihi eski şehir ve deniz ürünleri"
 },
   {
     name: "Marmaris",
     image: "https://www.yachttogo.com/wp-content/cache/seraphinite-accelerator/s/m/d/img/8a66bd41b264c342b90ed3efdfb06d17.1f2f6.jpeg?w=800",
-    discount: "%18",
+    discount: "%20",
     hotels: 89,
     description: "Canlı gece hayatı ve muhteşem koylar"
   },
   {
     name: "Bali",
     image: "https://www.royaltur.com/images/tour/1308_vitrin_725911.jpg?w=800",
-    discount: "%28",
+    discount: "%35",
     hotels: 24,
     description: "Tapınaklar, pirinç tarlaları ve sörf cenneti"
   }
@@ -104,9 +104,9 @@ export const cardTiers = [
     price: "₺5599/yıl",
     color: "from-amber-400 to-yellow-500",
     features: [
-      "Otellerde %20 indirim",
+      "Otellerde %40 indirim",
       "Beach club ücretsiz giriş",
-      "Restoranlarda %15 indirim",
+      "Restoranlarda %25 indirim",
       "Yıllık 200.000₺ harcama limiti",
       "Seyahat sigortası",
       "Havalimanı lounge 6 kez/yıl"
@@ -118,14 +118,14 @@ export const cardTiers = [
     price: "₺9599/yıl",
     color: "from-slate-300 to-slate-500",
     features: [
-      "Otellerde %30 indirim",
+      "Otellerde %60 indirim",
       "Beach club premium üyelik",
-      "Restoranlarda %20 indirim",
+      "Restoranlarda %40 indirim",
       "Sınırsız harcama limiti",
       "Premium seyahat sigortası",
       "Sınırsız VIP lounge",
       "Özel concierge hizmeti",
-      "Yat kiralama %15 indirim"
+      "Yat kiralama %25 indirim"
     ],
     popular: false
   }
